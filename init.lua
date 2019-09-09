f9f5339e71b4768cc035b58da7b785d294fe1beb
NatureFreshMilk <naturefreshmilk@github.com>
2019-09-09 15:23:06 +0200

    remove hacky overrides

--- init.lua
+++ init.lua
@@ -10,7 +10,6 @@ dofile(MP.."/api_action_on.lua")
 dofile(MP.."/api_nodetimer.lua")
 dofile(MP.."/register.lua")
 dofile(MP.."/flush.lua")
-dofile(MP.."/globalstep.lua")
-dofile(MP.."/actionqueue.lua")
+
 
 print("[OK] mesecons_debug loaded")
