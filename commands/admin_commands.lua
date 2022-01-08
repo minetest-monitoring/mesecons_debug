bb5fbf2ba292169e3eff0b9a0f022133dff4f359
flux <25628292+fluxionary@users.noreply.github.com>
2022-01-08 13:30:42 -0800

    add command to update settings

--- commands/admin_commands.lua
+++ commands/admin_commands.lua
@@ -1,62 +1,3 @@
-minetest.register_chatcommand("mesecons_hud", {
-    description = "mesecons_hud toggle",
-    func = function(name)
-        mesecons_debug.hud[name] = not mesecons_debug.hud[name] or nil
-        if mesecons_debug.hud[name] then
-            return true, "mesecons hud enabled"
-        else
-            return true, "mesecons hud disabled"
-        end
-    end
-})
-
-minetest.register_chatcommand("mesecons_global_stats", {
-    description = "shows the global mesecons stats",
-    func = function()
-        local top_ctx, top_hash
-
-        for hash, ctx in pairs(mesecons_debug.context_store) do
-            if not top_ctx or top_ctx.avg_micros < ctx.avg_micros then
-                -- store context with the most average time
-                top_ctx = ctx
-                top_hash = hash
-            end
-        end
-
-        local txt
-        if top_ctx then
-            txt = (
-                "Most prominent mesecons usage at mapblock %s" ..
-                " with %f seconds penalty and %i us average use"
-            ):format(
-                minetest.pos_to_string(minetest.get_position_from_hash(top_hash)),
-                top_ctx.penalty,
-                top_ctx.avg_micros
-            )
-        else
-            txt = "no context available"
-        end
-
-        return true, txt
-    end
-})
-
-minetest.register_chatcommand("mesecons_stats", {
-    description = "shows some mesecons stats for the current position",
-    func = function(name)
-        local player = minetest.get_player_by_name(name)
-        if not player then
-            return
-        end
-
-        local ctx = mesecons_debug.get_context(player:get_pos())
-        return true, ("Mapblock usage: %i us/s (across %i mapblocks)"):format(
-            ctx.avg_micros,
-            mesecons_debug.context_store_size
-        )
-    end
-})
-
 minetest.register_chatcommand("mesecons_enable", {
     description = "enables the mesecons globlastep",
     privs = { mesecons_debug = true },
@@ -132,5 +73,31 @@ minetest.register_chatcommand("mesecons_whitelist_remove", {
         mesecons_debug.storage:set_string(hash, "")
 
         return true, "mapblock removed from whitelist"
+    end
+})
+
+minetest.register_chatcommand("mesecons_debug_setting", {
+    description = "modify mesecons_debug settings",
+    params = "<setting> <value>",
+    privs = { mesecons_debug = true },
+    func = function(name, params)
+        local player = minetest.get_player_by_name(name)
+        if not player or not params then
+            return false
+        end
+
+        local setting, value = params:match('^([a-zA-Z0-9_-]+)%s+(.*)$')
+        value = tonumber(value)
+        if not setting or not value then
+            return false
+        end
+
+        if not mesecons_debug.settings[setting] then
+            return false, "unknown setting"
+        end
+
+        mesecons_debug.settings.modify_setting(setting, value)
+
+        return true, "setting updated"
     end
 })
