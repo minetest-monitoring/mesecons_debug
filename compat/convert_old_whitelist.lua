6415cbfd1682e57fbc7ea3e7475e7a96cdf76a3b
flux <25628292+fluxionary@users.noreply.github.com>
2022-01-22 12:12:36 -0800

    fix whitelist conversion

--- compat/convert_old_whitelist.lua
+++ compat/convert_old_whitelist.lua
@@ -2,7 +2,7 @@ local filename = minetest.get_worldpath() .. "/mesecons_debug_whiltelist"
 local file = io.open(filename, "r")
 
 if file then
-    local data = file:read("r")
+    local data = file:read("*a")
     local whitelist = minetest.deserialize(data) or {}
     for hash, _ in pairs(whitelist) do
         mesecons_debug.storage:set_string(hash, "1")
